6523dc9abec120f663e912adadc928ed72582aa5
phenix <dalibor.jaros@starnet.cz>
2016-11-15 22:39:22 +0100

    toast message

--- src/pages/new-todo/new-todo.ts
+++ src/pages/new-todo/new-todo.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController, Toast} from 'ionic-angular';
+import {NavController, Toast, ToastController} from 'ionic-angular';
 import {Data} from '../../providers/data'
 import {Todo} from "../../models/todo";
 
@@ -17,7 +17,7 @@ export class NewTodoPage {
 
     todo: Todo;
 
-    constructor(public navCtrl: NavController, public _data: Data) {
+    constructor(public navCtrl: NavController, public _data: Data, public toastCtrl: ToastController) {
         this.todo = new Todo();
     }
 
@@ -28,8 +28,16 @@ export class NewTodoPage {
     save() {
         var key = this._data.save(this.todo);
         if (key) {
-            console.log('saved');
+            let toast = this.toastCtrl.create({
+                message: 'Todo SAVED!',
+                duration: 3000
+            });
+            toast.present();
+
+            this.navCtrl.pop();
+
         }
+
     }
 
 }
